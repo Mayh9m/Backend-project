31ea956592f669c8e5950485d5cd2d6b7130f4af
Mayh9m <mahimrawal@gmail.com>
2025-12-10 15:56:06 +0545

    Fix refresh access token validation logic to check for absence of token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,7 +146,7 @@ try {
     const refreshAccessToken = asyncHandler (async(req,res)=> {
         const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
     
-        if(incomingRefreshToken){
+        if(!incomingRefreshToken){
             throw new ApiError(401,"unauthorized request")
         }
         const decodedToken = jwt.verify(
