b55d3879409147c91b72bb2f1f800baeba968837
Mayh9m <mahimrawal@gmail.com>
2025-12-10 21:35:04 +0545

    watchhistory aggregation done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -361,6 +361,50 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
    .json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
 })
 
+const getUserWatchHistory = asyncHandler(async (req,res) =>{
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id :new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[{
+                    $lookup: {
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as: "owner",
+                        pipeline: [
+                            {
+                                $project:{
+                                    fullName:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $first:"$owner"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }]
+            }
+        }
+    ])
+    return res.status(200)
+    .json(new ApiResponse(200,user[0].watchHistory,"watch history successfully retrieved"))
+})
+
 export {    registerUser ,                     loginUser  ,
                     logoutUser , 
                     refreshAccessToken,
@@ -368,4 +412,6 @@ export {    registerUser ,                     loginUser  ,
                     getCurrentUser,
                     updateAccountDetails,
                     updateUserAvatar,
-                    updateUserCoverImage    };
+                    updateUserCoverImage ,
+                getUserChannelProfile,
+            getUserWatchHistory   };
